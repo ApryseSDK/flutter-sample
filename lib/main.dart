10ff2bfd0e35fa472a5d5965bcfefa328ec0e547
Maxwell-Yang-PDFTron <myang@pdftron.com>
2021-01-08 13:47:40 -0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:permission_handler/permission_handler.dart';
 void main() => runApp(MyApp());
 
 const ASPECT_RATIO = 1224 / 1584;
-const SPACING = 2.0;
+const MARGIN = 5.0;
 
 const PORTRAIT_NUM_COLUMNS = 2;
 const LANDSCAPE_NUM_COLUMNS = 3;
@@ -74,27 +74,46 @@ class _MyAppState extends State<MyApp> {
 
   Widget getBody() {
     if (_initialized && _storagePermitted) {
-      return OrientationBuilder(builder: (context, orientation) {
-        return GridView.builder(
-          itemCount: thumbnailList.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: orientation == Orientation.portrait
-                  ? PORTRAIT_NUM_COLUMNS
-                  : LANDSCAPE_NUM_COLUMNS,
-              crossAxisSpacing: SPACING,
-              mainAxisSpacing: SPACING,
-              childAspectRatio: ASPECT_RATIO),
-          itemBuilder: (BuildContext context, int index) {
-            Thumbnail thumbnail = thumbnailList[index];
-            return InkWell(
-              onTap: () {
-                openDocument(thumbnail.documentUrl);
-              },
-              child: Image.asset(thumbnail.assetPath, fit: BoxFit.fitWidth),
-            );
-          },
-        );
-      });
+      return SafeArea(child: Container(
+        child: OrientationBuilder(builder: (context, orientation) {
+          return GridView.builder(
+            itemCount: thumbnailList.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: orientation == Orientation.portrait
+                    ? PORTRAIT_NUM_COLUMNS
+                    : LANDSCAPE_NUM_COLUMNS,
+                // crossAxisSpacing: SPACING,
+                // mainAxisSpacing: SPACING,
+                childAspectRatio: ASPECT_RATIO),
+            itemBuilder: (BuildContext context, int index) {
+              Thumbnail thumbnail = thumbnailList[index];
+              return InkWell(
+                  child: Container(
+                    margin: EdgeInsets.all(MARGIN),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(thumbnail.assetPath),
+                        fit: BoxFit.fitWidth,
+                      ),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          spreadRadius: 1,
+                          blurRadius: 3,
+                          offset: Offset(2, 2), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Image(image: AssetImage(thumbnail.assetPath)),
+                  ),
+                  onTap: () {
+                    openDocument(thumbnail.documentUrl);
+                  });
+            },
+          );
+        }),
+      ));
     } else {
       return Container(
           child: Align(
--- lib/thumbnail.dart
+++ lib/thumbnail.dart
@@ -7,20 +7,26 @@ class Thumbnail {
 }
 
 final thumbnailList = [
-  Thumbnail('https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_about.pdf',
-      'assets/pdftron-about-cover.png'),
+  Thumbnail('https://www.pdftron.com/webviewer/demo/gallery/PDFTRON_about.pdf',
+      'assets/file-covers/about-cover.png'),
+  Thumbnail('https://www.pdftron.com/webviewer/demo/gallery/Report_2011.pdf',
+      'assets/file-covers/report-cover.png'),
+  Thumbnail('https://www.pdftron.com/webviewer/demo/gallery/floorplan.pdf',
+      'assets/file-covers/floorplan-cover.png'),
   Thumbnail(
-      'https://pdftron.s3.amazonaws.com/custom/ID-zJWLuhTffd3c/sgong/getting_started.pdf',
-      'assets/getting-started-cover.png'),
-  Thumbnail('http://www.africau.edu/images/default/sample.pdf',
-      'assets/simple-cover.png'),
-  Thumbnail('https://www.hq.nasa.gov/alsj/a17/A17_FlightPlan.pdf',
-      'assets/large-cover.png'),
+      'https://www.pdftron.com/webviewer/demo/gallery/chart_supported.pdf',
+      'assets/file-covers/chart-cover.png'),
+  Thumbnail('https://www.pdftron.com/webviewer/demo/gallery/form-1040.pdf',
+      'assets/file-covers/form-cover.png'),
+  Thumbnail('https://www.pdftron.com/webviewer/demo/gallery/magazine.pdf',
+      'assets/file-covers/magazine-cover.png'),
   Thumbnail(
-      'https://ocw.mit.edu/courses/mathematics/18-821-project-laboratory-in-mathematics-spring-2013/writing/MIT18_821S13_latexsample.pdf',
-      'assets/math-cover.png'),
-  Thumbnail('https://planetpdf.com/planetpdf/pdfs/jpn.pdf',
-      'assets/japanese-cover.png'),
+      'https://www.pdftron.com/webviewer/demo/gallery/comp_sci_cheatsheet.pdf',
+      'assets/file-covers/cheatsheet-cover.png'),
   Thumbnail(
-      'https://www.irs.gov/pub/irs-pdf/f1040.pdf', 'assets/form-cover.png')
+      'https://www.pdftron.com/webviewer/demo/gallery/legal-contract-doc.pdf',
+      'assets/file-covers/contract-cover.png'),
+  Thumbnail(
+      'https://www.pdftron.com/webviewer/demo/gallery/construction_drawing-final.pdf',
+      'assets/file-covers/layout-cover.png'),
 ];
