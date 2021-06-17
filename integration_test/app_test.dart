360fb0f5a6af3f6925051a62beae0b8d0994d8cd
akwanpdf <akwan@pdftron.com>
2021-06-17 13:38:39 -0700

    Integration testing

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:flutter_sample/thumbnail.dart';
 
 import 'package:flutter_sample/main.dart' as app;
 
@@ -18,7 +19,7 @@ void main() {
     Widget myApp = app.MyApp();
     await tester.pumpWidget(myApp);
 
-    //expect(find.text('Storage permission required.'), findsOneWidget);
+    expect(find.text('PDFTron Flutter Sample'), findsOneWidget);    
 
     final materialAppFinder = find.descendant(of: find.byWidget(myApp), matching: find.byType(MaterialApp));
     expect(materialAppFinder, findsOneWidget);
@@ -34,20 +35,47 @@ void main() {
   });
   /*
   testWidgets('Finds child instances when state initialized and permission granted', (WidgetTester tester) async {
+    var orientationBuilderWidget = OrientationBuilder(builder: (context, orientation) {
+
+    }
+    );
+    await tester.pumpWidget(InkWell(child: orientationBuilderWidget));
+    expect(find.byWidget(orientationBuilderWidget), findsOneWidget);
   });
+  
+  testWidgets('Finds child container instance of InkWell', (WidgetTester tester) async {
+    // underscored fields/classes/methods are only available in the .dart file where they are defined
+    int index = 0;
+    Thumbnail thumbnail = thumbnailList[index];
+    var inkWellContainerWidget = Container(
+      margin: EdgeInsets.all(app.MARGIN),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(thumbnail.assetPath),
+          fit: BoxFit.fitWidth,
+        ),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 1,
+            blurRadius: 3,
+            offset: Offset(2, 2), // changes position of shadow
+          ),
+        ]),
+      );
+      await tester.pumpWidget(InkWell(child: inkWellContainerWidget));
+      expect(find.byWidget(inkWellContainerWidget), findsOneWidget);
+  });*/
 
   testWidgets('Finds child instances when state not initialized or permission denied', (WidgetTester tester) async {
-    const textWidget = Text('Storage permission required.');
-    // Provide the textWidget to Align.
+    const textWidget = Text('Storage permission required.', textDirection: TextDirection.ltr);
     await tester.pumpWidget(Align(alignment: Alignment.center, child: textWidget));
-    // Search for the textWidget in the tree and verify it exists.
     expect(find.byWidget(textWidget), findsOneWidget);
 
-    const alignWidget = Align(alignment: Alignment.center, child: Text('Storage permission required.'));
-    // Provide the alignWidget to the Container.
+    const alignWidget = Align(alignment: Alignment.center, child: textWidget);
     await tester.pumpWidget(Container(child: alignWidget));
-    // Search for the alignWidget in the tree and verify it exists.
     expect(find.byWidget(alignWidget), findsOneWidget);
   });
-  */
+  
 }
